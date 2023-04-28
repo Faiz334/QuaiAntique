cb757ce919612ec2a4470d7e0af2d46bdd246c5d
Faiz334 <olfaiz@hotmail.fr>
2023-04-28 17:47:40 +0200

    dashboard + templates

--- assets/app.js
+++ assets/app.js
@@ -8,6 +8,13 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 
+import '@fortawesome/fontawesome-free/css/all.css';
+
+
+import jquery from 'jquery';
+const $ = require('jquery');
+global.$ = global.jQuery = $;
+ 
 import { Tooltip, Toast, Popover } from 'bootstrap';
 
 // start the Stimulus application
